f1b4ecd9e3d1b884dbde216b1118f0a214a967b4
yavwa <kmungasia@yahoo.com>
2016-08-16 21:11:14 +0300

    Honeypot technique

---
+++ honeypot/Honeyfarm.java
@@ -0,0 +1,116 @@
+// Copyright (c) 2016 Voxy Capital
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+package honeypot;
+
+import com.argo.util.TextFileHandler;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+public class Honeyfarm {
+	private static final String potListPath = "plugins/Honeypot/list.ncsv";
+	private static final String logPath = "plugins/Honeypot/honeypot.log";
+
+	private static List<Location> pots = new ArrayList<Location>();
+	private static List<String> potSelectUsers = new ArrayList<String>();
+
+	public static boolean refreshData(Honeypot plugin) {
+		World potWorld;
+		
+		TextFileHandler r = new TextFileHandler(potListPath);
+		pots.clear();
+		try {
+			List<String> list = r.readLines();
+			String[] coord;
+			while(!list.isEmpty()) {
+				coord = list.remove(0).split(",");
+				if(coord.length == 4) {
+					String world = coord[0];
+					if (plugin.getServer().getWorld(world) == null)
+						potWorld = plugin.getServer().getWorlds().get(0);
+					else
+						potWorld = plugin.getServer().getWorld(world);
+
+					pots.add(new Location(potWorld, new Double(coord[1]), new Double(coord[2]), new Double(coord[3])));
+				} else {
+					// Earlier than version 7 - No world set, have to assume primary world. 
+					String potWorldname = plugin.getServer().getWorlds().get(0).getName();
+					potWorld = plugin.getServer().getWorld(potWorldname);
+					pots.add(new Location(potWorld, new Double(coord[0]), new Double(coord[1]), new Double(coord[2])));
+				}
+			}
+
+		} catch (FileNotFoundException ex) {
+			return false;
+		} catch (IOException ex) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean saveData() {
+		TextFileHandler r = new TextFileHandler(potListPath);
+
+		List<String> tmp = new ArrayList<String>();
+
+		Location loc;
+		while(!pots.isEmpty()) {
+			loc = pots.remove(0);
+			tmp.add(loc.getWorld().getName() + "," + loc.getX() + "," + loc.getY() + "," + loc.getZ());
+		}
+		try {
+			r.writeLines(tmp);
+		} catch (IOException ex) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void log(String line) {
+		TextFileHandler r = new TextFileHandler(logPath);
+		try {
+			r.appendLine("[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "] " + line);
+		} catch (IOException ex) {}
+	}
+
+	public static boolean isPot(Location loc) {
+		if(pots.contains(loc)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void createPot(Location loc) {
+		if(!isPot(loc)) {
+			pots.add(loc);
+		}
+	}
+
+	public static void removePot(Location loc) {
+		pots.remove(loc);
+	}
+
+	public static void setPotSelect(Player player, boolean state) {
+		if(state) {
+			potSelectUsers.add(player.getName());
+		} else {
+			potSelectUsers.remove(player.getName());
+		}
+	}
+
+	public static boolean getPotSelect(Player player) {
+		if(potSelectUsers.contains(player.getName()))
+			return true;
+		else
+			return false;
+	}
+}
---
+++ honeypot/Honeypot.java
@@ -0,0 +1,71 @@
+// Copyright (c) 2016 Voxy Capital
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+package honeypot;
+
+import com.argo.bukkit.util.BansHandler;
+import java.io.File;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Honeypot extends JavaPlugin {
+	private final HoneypotBlockListener blockListener = new HoneypotBlockListener(this);
+	private final HoneypotPlayerListener playerListener = new HoneypotPlayerListener(this);
+
+
+	public void onEnable() {
+		createDirs();
+
+		if(!Settings.load()) {
+			System.out.println("Honeypot: an error occured while trying to load the properties file.");
+		}
+		if(!Honeyfarm.refreshData(this)) {
+			System.out.println("Honeypot: an error occured while trying to load the honeypot list.");
+		}
+		if(!HoneypotPermissionsHandler.setupPermissions(this)) {
+			System.out.println("Honeypot: Permissions plugin not found, using default.");
+		} else {
+			System.out.println("Honeypot: Permissions plugin found, using that.");
+		}
+
+		switch(BansHandler.setupbanHandler(this)) {
+		case NONE:
+			System.out.println("Honeypot: Didn't find MCBans plugin, using default.");
+			break;
+		case MCBANS:
+			System.out.println("Honeypot: MCBans plugin found, using that.");
+			break;
+		case SIMPLEBAN:
+			System.out.println("Honeypot: SimpleBan plugin found, using that.");
+			break;
+		default:
+			System.out.println("Honeypot: Didn't find MCBans plugin, using default.");
+			break;
+		}
+
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Type.PLAYER_INTERACT, playerListener, Priority.Low, this);
+		pm.registerEvent(Type.BLOCK_BREAK, blockListener, Priority.Highest, this);
+		getCommand("honeypot").setExecutor(new CmdHoneypot(this));
+
+		PluginDescriptionFile pdf = this.getDescription();
+		System.out.println(pdf.getName() + " revision " + pdf.getVersion() + " by " + pdf.getAuthors().get(0) + " succesfully loaded.");
+	}
+
+	public void onDisable() {
+		if(!Honeyfarm.saveData()) {
+			System.out.println("Honeypot: an error occured while trying to save the honeypot list.");
+		}
+
+		PluginDescriptionFile pdf = this.getDescription();
+		System.out.println(pdf.getName() + " revision " + pdf.getVersion() + " by " + pdf.getAuthors().get(0) + " succesfully disabled.");
+	}
+
+	public void createDirs() {
+		new File("plugins/Honeypot").mkdir();
+	}
+}
