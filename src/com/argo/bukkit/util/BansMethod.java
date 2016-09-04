56e0a87ecd090c99e823d63697af7fd2388f3060
yavwa <kmungasia@yahoo.com>
2016-09-04 10:55:14 +0300

    shilling

---
+++ src/com/argo/bukkit/util/BansMethod.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2016 Voxy Capital
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+package com.argo.bukkit.util;
+
+public enum BansMethod {
+	MCBANS,
+	SIMPLEBAN,
+	NONE
+}
---
+++ src/honeypot/CmdHoneypot.java
@@ -0,0 +1,48 @@
+// Copyright (c) 2016 Voxy Capital
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+package honeypot;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CmdHoneypot implements CommandExecutor {
+	private static Honeypot plugin;
+
+	public CmdHoneypot(Honeypot instance) {
+		plugin = instance;
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdName, String[] args) {
+		if(args.length == 0) {
+			if(sender instanceof Player) {
+				Player player = (Player)sender;
+				if(!HoneypotPermissionsHandler.canUseCmd(player)) {
+					player.sendMessage(ChatColor.RED + "You are not allowed to use this command.");
+				} else {
+					if(Honeyfarm.getPotSelect(player)) {
+						player.sendMessage(ChatColor.GREEN + "Honeypot creation cancelled.");
+						Honeyfarm.setPotSelect(player, false);
+					} else {
+						player.sendMessage(ChatColor.GREEN + "Right click a block with a " + Settings.getToolId() + " to create a honeypot. When finished, use /hp again.");
+						Honeyfarm.setPotSelect(player, true);
+					}
+				}
+			} else {
+				sender.sendMessage("Sorry, this command can only be used by players.");
+			}
+		} else if(args.length == 1) {
+			if(args[0].equalsIgnoreCase("save") || args[0].equalsIgnoreCase("s")) {
+				sender.sendMessage(ChatColor.GREEN + "Saving honeypot data...");
+				if(!Honeyfarm.saveData()) {
+					sender.sendMessage(ChatColor.DARK_RED + "Failed to save data.");
+				}
+			}
+		}
+		return true;
+	}
+}
